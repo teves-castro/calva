c7bdc3b267d58440b22a7ce2928e34a4d77bb331
Pedro Girardi <pedro.rogerio.girardi@gmail.com>
2018-05-13 08:57:10 +0700

    Connect and Disconnect commands working

--- src/main/calva/v2/cmd.cljs
+++ src/main/calva/v2/cmd.cljs
@@ -1,14 +1,28 @@
-(ns calva.v2.cmd)
-
+(ns calva.v2.cmd
+  (:require [calva.v2.db :refer [*db]]
+            [calva.repl.nrepl :as nrepl]))
 
 (defn ^{:cmd "calva.v2.connect"} connect []
-  (js/console.log "Connect"))
-
+  (let [^js socket (nrepl/connect #js {:host "localhost"
+                                       :port "64772"
+                                       :on-connect #(swap! *db
+                                                           (fn [m1 m2]
+                                                             (merge-with merge m1 m2))
+                                                           {:conn {:connected? true}})
+                                       :on-end #(swap! *db (fn [m]
+                                                             (let [^js socket (get-in m [:conn :socket])]
+                                                               (-> m
+                                                                   (update-in [:conn] dissoc :socket)
+                                                                   (assoc-in [:conn :connected?] false)))))})]
+    (swap! *db
+           (fn [m1 m2]
+             (merge-with merge m1 m2))
+           {:conn {:socket socket}})))
 
 (defn ^{:cmd "calva.v2.disconnect"} disconnect []
-  (js/console.log "Disconnect"))
-
+  (let [^js socket (get-in @*db [:conn :socket])]
+    (.end socket)))
 
 (defn ^{:cmd "calva.v2.state"} state []
-  (js/console.log "State"))
+  (js/console.log "State" (clj->js @*db)))
 
