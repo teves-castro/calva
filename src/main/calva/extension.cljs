63e1b3616140d731746d7e0408a8c3775dae9827
Pedro Girardi <pedro.rogerio.girardi@gmail.com>
2018-05-14 10:41:18 +0700

    Commands are db mutations, so we can compose then

--- src/main/calva/extension.cljs
+++ src/main/calva/extension.cljs
@@ -1,32 +1,38 @@
 (ns calva.extension
   (:require
    ["vscode" :as vscode]
-   
+
    [calva.v2.language :as language]
    [calva.v2.db :as db]
    [calva.v2.output :as output]
    [calva.v2.cmd :as cmd]))
 
-(defn- register-command [context *db cmd]
-   (-> (.-subscriptions context)
-       (.push (-> vscode
-                  (.-commands)
-                  (.registerCommand (-> cmd meta :cmd) #(cmd *db))))))
+(defn- register-command [context cmd]
+  (-> (.-subscriptions context)
+      (.push (-> vscode
+                 (.-commands)
+                 (.registerCommand (-> cmd meta :cmd) #(db/mutate! (fn [db]
+                                                                     (cmd db))))))))
 
 (defn activate [^js context]
   (-> (.-languages vscode)
       (.setLanguageConfiguration "clojure" (language/ClojureLanguageConfiguration.)))
 
-  (db/mutate! db/*db (fn [db]
-                       (assoc db :output (-> (.-window vscode)
-                                             (.createOutputChannel "Calva")))))
-  
-  (let [register-command (partial register-command context db/*db)]
-    (register-command #'cmd/connect)
-    (register-command #'cmd/disconnect)
-    (register-command #'cmd/state))
+  (register-command context #'cmd/connect)
+  (register-command context #'cmd/disconnect)
+  (register-command context #'cmd/state)
+  (register-command context #'cmd/connect-then-disconnect)
 
-  (output/append-line (:output @db/*db) "Calva is active."))
+  ;; Initialize db
+  (db/mutate! (fn [db]
+                (let [^js output (-> (.-window vscode)
+                                     (.createOutputChannel "Calva"))
+
+                      new-db (assoc db :output output)]
+
+                  (output/append-line output "Calva is active.")
+
+                  new-db))))
 
 (defn exports []
   #js {:activate activate})
