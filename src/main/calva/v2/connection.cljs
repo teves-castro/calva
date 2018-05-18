d84222b37a7116fa3ccb7b543a128f2d7597e946
Peter Strömberg <pez@pezius.com>
2018-05-18 09:20:48 +0200

    Take care of prompts inside repl-connect side-effect

--- src/main/calva/v2/connection.cljs
+++ src/main/calva/v2/connection.cljs
@@ -6,7 +6,8 @@
    [calva.v2.gui :as gui]))
 
 (def initial-state
-  {:connected? false})
+  {:connected? false
+   :connecting? false})
 
 (defmulti control (fn [event & args] event))
 
@@ -14,7 +15,9 @@
   {:state initial-state})
 
 (defmethod control :on-connect [r _ state]
-  (let [new-state (assoc state :connected? true)]
+  (let [new-state (assoc state
+                         :connected? true
+                         :connecting? false)]
     {:state new-state
      :output-line {:line (str "on-connect: " (pr-str new-state))}}))
 
@@ -23,23 +26,30 @@
     {:state new-state
      :output-line {:line (str "on-end: " (pr-str new-state))}}))
 
-(defmethod control :got-socket [r [^js socket] state]
-  {:state
-   (assoc state
-          :socket socket)})
+(defmethod control :save-socket [r [^js socket] state]
+  (let [new-state (assoc state :socket socket)]
+    {:state
+     new-state
+     :output-line {:line (str "save-socket: " (pr-str new-state))}}))
 
-(defmethod control :connect [r [host port] state]
+(defmethod control :save-host-and-port [r [host port] state]
   (let [new-state (assoc state
                          :host host
                          :port port)]
     {:state
      new-state
+     :output-line
+     {:line (str "save-host-and-port: " (pr-str state))}}))
+
+(defmethod control :connect [r _ state]
+  (let [new-state (assoc state
+                         :connecting? true)]
+    {:state
+     new-state
      :nrepl-connect
-     {:host host
-      :port port
-      :on-connect :on-connect
+     {:on-connect :on-connect
       :on-end :on-end
-      :got-socket :got-socket}
+      :save-socket :save-socket}
      :output-line
      {:line
       (str "connect: " (pr-str new-state))}}))
@@ -50,27 +60,27 @@
    :output-line
    {:line (str "disconnect: " (pr-str state))}})
 
-(defn nrepl-connect [r cn {:keys [host port on-connect on-end got-socket]}]
-  (when (and host port)
-    (let [^js socket (nrepl/connect {:host host
-                                     :port port
-                                     :on-connect #(citrus/dispatch! r cn on-connect)
-                                     :on-end #(citrus/dispatch! r cn on-end)})]
-      (citrus/dispatch! r cn got-socket socket))))
-
-(defn socket-disconnect [r cn {:keys [socket]}]
-  (when socket
-    (.end socket)))
-
-(defn ^{:cmd "calva.v2.connect"} connect-cmd [r]
+(defn nrepl-connect [r cn {:keys [on-connect on-end save-socket]}]
   (p/let [host (gui/show-input-box {:placeHolder "nREPL Server Address"
                                     :ignoreFocusOut true
                                     :value "localhost"})
 
           port (gui/show-input-box {:placeHolder "nREPL Server Port"
                                     :ignoreFocusOut true})]
-    (p/->> (p/all [host port])
-           (apply citrus/dispatch! r :connection :connect))))
+    (when (and host port)
+      (citrus/dispatch! r cn :save-host-and-port host port)
+      (->> (nrepl/connect {:host host
+                           :port port
+                           :on-connect #(citrus/dispatch! r cn on-connect)
+                           :on-end #(citrus/dispatch! r cn on-end)})
+           (citrus/dispatch! r cn save-socket)))))
+
+(defn socket-disconnect [r cn {:keys [socket]}]
+  (when socket
+    (.end socket)))
+
+(defn ^{:cmd "calva.v2.connect"} connect-cmd [r]
+  (citrus/dispatch! r :connection :connect))
 
 (defn ^{:cmd "calva.v2.disconnect"} disconnect-cmd [r]
   (citrus/dispatch! r :connection :disconnect))
@@ -78,7 +88,7 @@
 (defn ^{:cmd "calva.v2.state"} state [r]
   (citrus/dispatch! r :output :output-line (str "connection state: " (pr-str @(citrus/subscription r [:connection])))))
 
+;;; This is not the way to do this. Obviously
 (defn ^{:cmd "calva.v2.connectThenDisconnect"} connect-then-disconnect [r]
-  (-> r
-      (connect-cmd)
-      (disconnect-cmd)))
+  (citrus/dispatch-sync! r :connection :connect)
+  (citrus/dispatch-sync! r :connection :disconnect))
